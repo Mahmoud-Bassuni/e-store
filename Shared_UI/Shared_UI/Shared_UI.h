31928d08104242c3da673911dd3ee311d9b8dc5c
Mahmoud Mohammed Bassiony <mamabdallah@banquemisr.com>
2023-03-09 20:57:32 +0200

    create new layer of shared ui

---
+++ Shared_UI/Shared_UI/Shared_UI.h
@@ -0,0 +1,18 @@
+//
+//  Shared_UI.h
+//  Shared_UI
+//
+//  Created by Bassuni on 09/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shared_UI.
+FOUNDATION_EXPORT double Shared_UIVersionNumber;
+
+//! Project version string for Shared_UI.
+FOUNDATION_EXPORT const unsigned char Shared_UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shared_UI/PublicHeader.h>
+
+
