7690928c4e484ff2df535112eb952112960113ca
Mahmoud Mohammed Bassiony <mamabdallah@banquemisr.com>
2023-03-09 20:44:57 +0200

    create new app create new target

---
+++ e-store/e-store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  e-store
+//
+//  Created by Bassuni on 09/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
