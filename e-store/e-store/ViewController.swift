fa5f7c317344abae0bfd408aa0b909e39047fa1c
ZeyadSultan <ziadkhaled2260.zk@gmail.com>
2023-03-11 23:49:49 +0200

    add swiftlint

--- e-store/e-store/ViewController.swift
+++ e-store/e-store/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        var value: String? = "s"
+        print(value!)
         
         // Do any additional setup after loading the view.
     }
