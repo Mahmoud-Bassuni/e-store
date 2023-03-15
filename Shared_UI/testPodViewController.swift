fa99e76656e94b7121e1093100a28bc1c3d66f43
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2023-03-15 23:23:50 +0200

    modify podfile to target e-store and shared_UI

---
+++ Shared_UI/testPodViewController.swift
@@ -0,0 +1,29 @@
+//
+//  testPodViewController.swift
+//  Shared_UI
+//
+//  Created by A1softech on 15/03/2023.
+//
+
+import UIKit
+import Alamofire
+class testPodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
