7a0d1c8abd0fc45547de74c37bd5d8baa4132a81
Ahmed Atef <91344134+AhmedAtef277@users.noreply.github.com>
2023-03-17 13:48:54 +0200

    Move colors to UIColor extension
    - Remove colors from assets
    - Add colors to UIColor extension

--- Shared_UI/Shared_UI/Extension/UIColor + Extension.swift
+++ Shared_UI/Shared_UI/Extension/UIColor + Extension.swift
@@ -7,23 +7,23 @@
 import Foundation
 import UIKit
 
-extension UIColor {
+public extension UIColor {
     static var blueColor: UIColor {
-        UIColor(named: "blue") ?? .blue
+        UIColor(red: 54/255, green: 105/255, blue: 201/255, alpha: 1)
     }
     static var lightBlueColor: UIColor {
-        UIColor(named: "lightBlue")!
+        UIColor(red: 239/255, green: 245/255, blue: 251/255, alpha: 1)
     }
     static var grayColor: UIColor {
-        UIColor(named: "gray") ?? .gray
+        UIColor(red: 196/255, green: 197/255, blue: 196/255, alpha: 1)
     }
     static var redColor: UIColor {
-        UIColor(named: "red") ?? .red
+        UIColor(red: 254/255, green: 58/255, blue: 48/255, alpha: 1)
     }
     static var whiteColor: UIColor {
-        UIColor(named: "white") ?? .white
+        UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
     }
     static var navyBlackColor: UIColor {
-        UIColor(named: "navyBlack") ?? .black
+        UIColor(red: 12/255, green: 26/255, blue: 48/255, alpha: 1)
     }
 }
