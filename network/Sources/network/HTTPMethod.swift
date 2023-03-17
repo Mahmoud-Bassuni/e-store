9dd8c5dba59a8e7d0b747632f93b98503127a6a2
Ahmed Atef <91344134+AhmedAtef277@users.noreply.github.com>
2023-03-18 00:03:41 +0200

    Add Network layer
    - Add baseRequest file
    - Add HttpMethod
    - Add ServiceLayer protocol that contain request requirements
    - Add Task enum that contain types of requests

---
+++ network/Sources/network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ahmed Atef on 17/03/2023.
+//
+
+import Foundation
+
+public enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
