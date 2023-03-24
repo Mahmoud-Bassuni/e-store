840956f34e1b65d2dc1b8ce471b08739ee449b28
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2023-03-24 03:05:11 +0200

    update file fetchUserUseCase in Domain Layer
    - update all closure in function by network logger

--- Domain/Sources/Domain/FetchUserUseCase.swift
+++ Domain/Sources/Domain/FetchUserUseCase.swift
@@ -6,23 +6,39 @@
 //
 
 import Foundation
+import Core
 public struct FetchUserUseCase {
     let userRepo = UserRepository()
     public init() {
     }
     public func fetchUserData( completion: @escaping ((User) -> Void) ) {
-        userRepo.fetchUserCodable {  userCodable, _ in
+        userRepo.fetchUserCodable { result in
+            switch result {
+            case .failure(let error):
+                print("Failure \(error)")
+            case .success(let userCodable):
                 completion(userCodable.toUser())
+            }
         }
-}
+    }
     public func fetchUsersData( completion: @escaping (([User]) -> Void) ) {
-        userRepo.fetchUsersCodable {  inuser, _ in
-            completion(inuser?.toUser() ?? [User()])
+        userRepo.fetchUsersCodable { result in
+            switch result {
+            case .success(let usersCodable):
+                completion(usersCodable.toUser())
+            case .failure(let error):
+                print("failure \(error)")
+            }
         }
     }
     public func fetchTokenLoginUser(loginInfo localLoginInfo: (userName:String,passWord:String), completion: @escaping (Token?, Error?) -> Void) {
-        userRepo.loginUser(loginInfo: localLoginInfo) { token, error in
-            completion(token,error)
+        userRepo.loginUser(loginInfo: localLoginInfo) { result in
+            switch result {
+            case .success(let token):
+                completion(token, nil)
+            case .failure(let error):
+                completion(nil, error)
+            }
         }
     }
 }
