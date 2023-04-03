fe8e53434b2515ce6730d7b66062bc3e878ec3f7
AhmedAtef277 <91344134+AhmedAtef277@users.noreply.github.com>
2023-04-04 00:57:12 +0200

    Add Store Assemply

---
+++ e-store/e-store/AppDependencies/StoreAssemply.swift
@@ -0,0 +1,26 @@
+//
+//  StoreAssemply.swift
+//  e-store
+//
+//  Created by Ahmed Atef on 04/04/2023.
+//
+
+import Foundation
+import Swinject
+
+class StoreAssemply: Assembly {
+    
+    //MARK: - Properties
+    let sharedContainer: Container
+        //MARK: - Lifecycle
+    init(sharedContainer: Container) {
+        self.sharedContainer = sharedContainer
+    }
+    //MARK: - Functions
+    func assemble(container: Swinject.Container) {
+        sharedContainer.register(LoginViewModel.self) { _ in
+            LoginViewModel()
+        }
+    }
+    
+}
