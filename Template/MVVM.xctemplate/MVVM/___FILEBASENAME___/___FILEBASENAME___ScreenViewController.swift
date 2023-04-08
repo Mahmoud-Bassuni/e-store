b48a7d1a65f06e772b5f501b311d1ef089973560
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2023-04-08 22:45:47 +0200

    add Template MVVM

---
+++ Template/MVVM.xctemplate/MVVM/___FILEBASENAME___/___FILEBASENAME___ScreenViewController.swift
@@ -0,0 +1,24 @@
+//  ___FILEHEADER___
+import UIKit
+
+class ___FILEBASENAME___: UIViewController {
+
+    //MARK: Outlets
+    
+    //MARK: Proprites
+   
+    //MARK: Init
+
+    //MARK: LifeCycle
+
+}
+
+//MARK: Bind view model
+extension ___FILEBASENAME___ {
+
+}
+
+//MARK: Actions
+extension ___FILEBASENAME___ {
+   
+}
---
+++ Template/MVVM.xctemplate/MVVM/___FILEBASENAME___/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,27 @@
+//  ___FILEHEADER___
+import Foundation
+
+// MARK: - Protocol___FILEBASENAME___Input
+protocol ___FILEBASENAME___Input {
+    
+}
+
+// MARK: - Protocol___FILEBASENAME___Output
+protocol ___FILEBASENAME___Output {
+    
+}
+
+// MARK: - ___FILEBASENAME___
+class ___FILEBASENAME___ {
+
+}
+
+// MARK: - ___FILEBASENAME___Input
+extension ___FILEBASENAME___: ___FILEBASENAME___Input {
+
+}
+
+// MARK: - ___FILEBASENAME___Input
+extension ___FILEBASENAME___: ___FILEBASENAME___Output {
+
+}
