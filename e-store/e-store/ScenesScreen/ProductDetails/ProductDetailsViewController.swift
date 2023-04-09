4696b301a9b5575dbda2825582be0ffe14b84457
AhmedAtef277 <91344134+AhmedAtef277@users.noreply.github.com>
2023-04-09 17:45:36 +0200

    Create product details screen
    - Add productImagesCollectionView
    - Add ProductImagesCollectionViewCell
    - Add productName label
    - Add productPrice label
    - Add productRate label
    - Add productReviewsNumber label
    - Add product images in Assets file

--- e-store/e-store/ScenesScreen/ProductDetails/ProductDetailsViewController.swift
+++ e-store/e-store/ScenesScreen/ProductDetails/ProductDetailsViewController.swift
@@ -10,13 +10,49 @@ import Shared_UI
 
 class ProductDetailsViewController: UIViewController {
     
+    // MARK: - Outlets
+    @IBOutlet private(set) weak var productImagesCollectionView: UICollectionView!
+    @IBOutlet private(set) weak var productName: UILabel!
+    @IBOutlet private(set) weak var productPrice: UILabel!
+    @IBOutlet private(set) weak var productRate: UILabel!
+    @IBOutlet private(set) weak var productReviewsNumber: UILabel!
     // MARK: - Properties
+    private var productImages: [UIImage] = []
+    private var timer: Timer?
+    private var currentIndex: Int = 0
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerCell()
+        setupProductImagesCollectionView()
         setupNavigationItems()
+        setupProductImagesList()
+        setupCollectionViewTimer()
+        setupUi()
     }
+    
     // MARK: - Functions
+    private func setupUi() {
+        productPrice.textColor = UIColor.scarletColor
+        [productRate, productReviewsNumber, productName].forEach {
+            $0?.textColor = UIColor.navyBlackColor
+        }
+    }
+    private func setupProductImagesCollectionView() {
+        productImagesCollectionView.dataSource = self
+        productImagesCollectionView.delegate = self
+    }
+    private func registerCell() {
+        productImagesCollectionView.register(UINib(nibName: ProductImagesCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: ProductImagesCollectionViewCell.identifier)
+    }
+    private func setupProductImagesList() {
+        productImages.append(UIImage(named: "applewatch-removebg-preview") ?? UIImage())
+        productImages.append(UIImage(named: "headphone-removebg-preview") ?? UIImage())
+        productImages.append(UIImage(named: "airpods-removebg-preview") ?? UIImage())
+        productImages.append(UIImage(named: "flower-removebg-preview") ?? UIImage())
+        productImages.append(UIImage(named: "car-removebg-preview") ?? UIImage())
+    }
     private func setupNavigationItems() {
         navigationItem.title = "Detail Product"
         let arrow = UIBarButtonItem(image: UIImage(systemName: "arrowshape.turn.up.right"), style: .plain, target: self, action: #selector(addTapped))
@@ -29,7 +65,37 @@ class ProductDetailsViewController: UIViewController {
     }
     
     @objc private func addTapped() {
-        //TODO: - Implement me 
+        // TODO:- Implement me
+    }
+    private func setupCollectionViewTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(moveToNext), userInfo: nil, repeats: true)
     }
     
+    @objc private func moveToNext() {
+        currentIndex = (currentIndex < productImages.count - 1) ? (currentIndex + 1) :  0
+        productImagesCollectionView.scrollToItem(at: IndexPath(row: currentIndex, section: 0), at: .centeredHorizontally, animated: true)
+    }
+}
+    // MARK: - ProductDetailsViewController extension
+extension ProductDetailsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        productImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductImagesCollectionViewCell.identifier, for: indexPath) as? ProductImagesCollectionViewCell {
+            cell.setupCell(productImage: productImages[indexPath.row], imagesCounter: indexPath.row + 1, totalImagesNumber: productImages.count)
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+}
+extension ProductDetailsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
 }
--- e-store/e-store/ScenesScreen/ProductDetails/ProductImagesCollectionViewCell/ProductImagesCollectionViewCell.swift
+++ e-store/e-store/ScenesScreen/ProductDetails/ProductImagesCollectionViewCell/ProductImagesCollectionViewCell.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ProductImagesCollectionViewCell: UICollectionViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    //MARK: - Outlets
+    @IBOutlet private(set)weak var productImage: UIImageView!
+    @IBOutlet private(set)weak var imagesCounter: UILabel!
+    @IBOutlet private(set)weak var totalImagesNumber: UILabel!
+    
+    //MARK: - Properties
+    static let identifier = String(describing: ProductImagesCollectionViewCell.self)
+    
+    //MARK: - Functions
+    func setupCell(productImage: UIImage, imagesCounter: Int, totalImagesNumber: Int) {
+        self.productImage.image = productImage
+        self.imagesCounter.text = "\(imagesCounter)"
+        self.totalImagesNumber.text = "/\(totalImagesNumber)"
     }
-
+    
 }
