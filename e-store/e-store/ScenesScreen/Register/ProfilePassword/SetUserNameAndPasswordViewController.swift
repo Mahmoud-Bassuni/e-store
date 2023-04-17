e0fd1e501c28c7105cb6ea8d6e75848ec3593b19
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2023-04-17 23:03:20 +0200

    update Name of screen profile and password

---
+++ e-store/e-store/ScenesScreen/Register/ProfilePassword/SetUserNameAndPasswordViewController.swift
@@ -0,0 +1,38 @@
+//  
+//  SetUserNameAndPasswordViewController.swift
+//  e-store
+//
+//  Created by Hassan on 10/04/2023.
+//
+import UIKit
+
+class SetUserNameAndPasswordViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    @IBOutlet weak var referalCodeTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    // MARK: Proprites
+   
+    // MARK: Init
+
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: Bind view model
+extension SetUserNameAndPasswordViewController {
+
+}
+
+// MARK: Actions
+extension SetUserNameAndPasswordViewController {
+   
+}
+// MARK: - Update TextField
+extension SetUserNameAndPasswordViewController {
+
+}
