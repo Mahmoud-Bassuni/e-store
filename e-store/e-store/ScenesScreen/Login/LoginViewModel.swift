9143d598cbe2aacf7639f5164257c67f63af85e1
Zeyad Khaled Sultan <ziadkhaled2260.zk@gmail.com>
2023-04-24 17:57:33 +0200

    - adding fns in the protocol

--- e-store/e-store/ScenesScreen/Login/LoginViewModel.swift
+++ e-store/e-store/ScenesScreen/Login/LoginViewModel.swift
@@ -14,6 +14,9 @@ protocol LoginViewModelInput {
 
 protocol LoginViewModelOutput {
     func checkConfigButton(callback: @escaping (Bool) -> Void)
+    func validUsername() -> Bool
+    func validPassword() -> Bool
+    func checkUser(completion:@escaping (LoginMessage) -> Void)
 }
 
 // MARK: LoginViewModel
@@ -66,7 +69,7 @@ extension LoginViewModel: LoginViewModelOutput {
 extension LoginViewModel {
     
     func checkUser(completion:@escaping (LoginMessage) -> Void) {
-        var checkUsernameAndPassword = checkUsernameAndPassword()
+        let checkUsernameAndPassword = checkUsernameAndPassword()
         if(!checkUsernameAndPassword) {
             completion(.validateError)
         } else {
@@ -89,8 +92,8 @@ extension LoginViewModel {
 extension LoginViewModel {
     
     func checkUsernameAndPassword() -> Bool {
-        var emailValid :Bool = validUsername()
-        var passwordValid :Bool = validPassword()
+        let emailValid :Bool = validUsername()
+        let passwordValid :Bool = validPassword()
         if( (emailValid && passwordValid) == false) {
             return false
        }
