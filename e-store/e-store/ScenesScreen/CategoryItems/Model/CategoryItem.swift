89514ebdbed99f65b4959164d2143b8ecec2ed1b
Zeyad Khaled Sultan <ziadkhaled2260.zk@gmail.com>
2023-04-25 00:07:38 +0200

    - fixing the price to be double

--- e-store/e-store/ScenesScreen/CategoryItems/Model/CategoryItem.swift
+++ e-store/e-store/ScenesScreen/CategoryItems/Model/CategoryItem.swift
@@ -12,11 +12,11 @@ import Kingfisher
 class CategoryItem {
     var imgURL: String
     var name: String
-    var price: Int
+    var price: Double
     var rating: Float
     var reviews: Int
     
-    init(img: String, name: String, price: Int, rating: Float, reviews: Int) {
+    init(img: String, name: String, price: Double, rating: Float, reviews: Int) {
         
         self.imgURL = img
         self.name = name
