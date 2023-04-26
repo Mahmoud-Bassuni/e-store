134745a7f49b5c2da6826fb5b5c7e64e057bdc4e
AhmedAtef277 <91344134+AhmedAtef277@users.noreply.github.com>
2023-04-26 13:51:27 +0200

    Create main tab bar controller
    - Add MainTabBarController
    - Add tab bar items images

---
+++ e-store/e-store/ScenesScreen/MainTabBarController/MainTabBarController.swift
@@ -0,0 +1,54 @@
+//
+//  MainTabBarController.swift
+//  e-store
+//
+//  Created by Ahmed Atef on 26/04/2023.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setControllers()
+        setTabBarItemsTitle()
+        setTabBarItemsImage()
+        setTabBarItemsColor()
+    }
+    
+    private func setControllers() {
+        let controllers = controllersToSet()
+        setViewControllers(controllers, animated: true)
+    }
+    private func controllersToSet() -> [UIViewController] {
+        // these controllers just for test
+        let productDetails = ProductDetailsViewController()
+        let verification = VerificationScreenViewController()
+        return [productDetails, verification]
+    }
+    private func setTabBarItemsTitle() {
+        let titles = ["HOME", "WISHLIST", "ORDER", "ACCOUNT"]
+        guard let items = tabBar.items else {return}
+        for item in 0..<items.count {
+            items[item].title = titles[item]
+        }
+    }
+ 
+    private func setTabBarItemsImage() {
+        let images: [UIImage] = [UIImage(named: Asset.home.name) ?? UIImage(),
+                                 UIImage(named: Asset.wishlist.name) ?? UIImage(),
+                                 UIImage(named: Asset.orders.name) ?? UIImage(),
+                                 UIImage(named: Asset.accountPicture.name) ?? UIImage()]
+        guard let items = tabBar.items else {return}
+        for item in 0..<items.count {
+            items[item].image = images[item]
+        }
+    }
+    
+    private func setTabBarItemsColor() {
+        tabBar.unselectedItemTintColor = UIColor.navyBlackColor
+        tabBar.tintColor = UIColor.mediumBlueColor
+    }
+}
