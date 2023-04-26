d38dad1c8aa5417f16c0166493c73dc8d3d6541f
AhmedAtef277 <91344134+AhmedAtef277@users.noreply.github.com>
2023-04-27 01:40:37 +0200

    Update SceneDelegate.swift

--- e-store/e-store/SceneDelegate.swift
+++ e-store/e-store/SceneDelegate.swift
@@ -12,10 +12,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-              guard let scene = (scene as? UIWindowScene) else { return }
-              window = UIWindow(windowScene: scene)
-              window?.rootViewController = HomeViewController()
-              window?.makeKeyAndVisible()
+        
+        guard (scene as? UIWindowScene) != nil else { return }
         
     }
 
