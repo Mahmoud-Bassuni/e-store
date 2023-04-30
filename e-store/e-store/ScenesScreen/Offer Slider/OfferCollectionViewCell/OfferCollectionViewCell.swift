47b46dcce6e2161989be0ba01557de19a0758598
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2023-04-30 18:51:14 +0300

    create collectionView offer and cell and add photo

---
+++ e-store/e-store/ScenesScreen/Offer Slider/OfferCollectionViewCell/OfferCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  OfferCollectionViewCell.swift
+//  e-store
+//
+//  Created by Hassan on 29/04/2023.
+//
+
+import UIKit
+
+class OfferCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ e-store/e-store/ScenesScreen/Offer Slider/OfferSlider.swift
@@ -0,0 +1,20 @@
+//
+//  OfferSlider.swift
+//  e-store
+//
+//  Created by Hassan on 29/04/2023.
+//
+
+import UIKit
+
+class OfferSlider: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
