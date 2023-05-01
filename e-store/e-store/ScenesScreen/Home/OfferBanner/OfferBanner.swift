e6b76b2d27508e24fac015bdf681c1d915ad6a4e
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2023-05-01 14:47:47 +0300

    add logic to offerBanner and update ui

--- e-store/e-store/ScenesScreen/Home/OfferBanner/OfferBanner.swift
+++ e-store/e-store/ScenesScreen/Home/OfferBanner/OfferBanner.swift
@@ -5,4 +5,37 @@
 //  Created by Hassan on 01/05/2023.
 //
 
-import Foundation
+import UIKit
+
+class OfferBanner: UIView {
+    
+    // MARK: - Outlets
+    @IBOutlet private var contentView: UIView!
+    @IBOutlet private weak var offerImage: UIImageView!
+    @IBOutlet private weak var offerName: UILabel!
+    
+    // MARK: - Properties
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+        updateViewUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+        updateViewUI()
+    }
+    // MARK: - Lify cycle
+    
+    // MARK: - Functions
+    private func commonInit() {
+        fromNib(type: OfferBanner.self)
+    }
+    private func updateViewUI() {
+        contentView.layer.cornerRadius = 10
+    }
+}
